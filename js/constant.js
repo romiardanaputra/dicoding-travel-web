0a7329c287345fbf24bcb58915d01c0df4f368cd
romiardanaputra <romiardana21@gmail.com>
2024-09-28 15:48:25 +0800

    feat(constant): Add constant.js file

---
+++ js/constant.js
@@ -0,0 +1,120 @@
+export const reccomendations = [
+  {
+    id: 1,
+    title: "Besakih Temple",
+    description: `argest and holiest temple complex in Bali, Indonesia. Located on the slopes of Mount Agung, it is often  referred to as the “Mother Temple” of Balinese Hinduism. `,
+    image: "./assets/images/bali-besakih.webp",
+    alt: "reccomendation-bali-besakih",
+  },
+  {
+    id: 2,
+    title: `Jatiluwih`,
+    description: `picturesque village in Bali, Indonesia, renowned for its expansive and stunning rice terraces. These terraces, covering over 600 hectares, are irrigated using the traditional Subak system, a UNESCO World Heritage site`,
+    image: "./assets/images/bali-jatiluwih.webp",
+    alt: "reccomendation-bali-jatiluwih",
+  },
+  {
+    id: 3,
+    title: "Taman Ayun Temple",
+    description: `Taman Ayun Temple, located in Mengwi, Bali, is a stunning 17th-century temple complex built by the Mengwi Kingdom12. Known as the “Beautiful Garden Temple,” it features expansive gardens, intricate architecture, and a large moat that gives the illusion of the temple floating on water13`,
+    image: "./assets/images/bali-taman-ayun.webp",
+    alt: "recomendation-bali-taman-ayun",
+  },
+  {
+    id: 4,
+    title: "Tanah Lot",
+    description:
+      "Tanah Lot is a rock formation off the Indonesian island of Bali. It is home to the ancient Hindu pilgrimage temple Pura Tanah Lot, a popular tourist and cultural icon for photography and general exoticism.",
+    image: "./assets/images/bali-tanah-lot.webp",
+    alt: "reccomendation-bali-tanah-lot",
+  },
+  {
+    id: 5,
+    title: `Rambut Siwi Temple`,
+    description: `The Rambut Siwi Temple is a religious temple located in the city of Ubud, Indonesia. It is one of the most popular religious sites in the country, with over 20,000 visitors annually.`,
+    image: "./assets/images/bali-rambut-siwi.webp",
+    alt: "recomendation-bali-rambut-siwi",
+  },
+
+  {
+    id: 6,
+    title: `Git Git Waterfall`,
+    description: `Git Git Waterfall is a waterfall located in the city of Ubud, Indonesia. It is one of the most popular religious sites in the country, with over 20,000 visitors annually.`,
+    image: `./assets/images/bali-gitgit-waterfall.webp`,
+    alt: "recomendation-bali-gitgit-waterfall",
+  },
+  {
+    id: 7,
+    title: `Monkey Forest`,
+    description: `Monkey Forest is a waterfall located in the city of Ubud, Indonesia. It is one of the most popular religious sites in the country, with over 20,000 visitors annually.`,
+    image: `./assets/images/bali-monkey-forest.webp`,
+    alt: "recomendation-bali-monkey-forest",
+  },
+  {
+    id: 8,
+    title: "Campuan Hill",
+    description: `Campuan Hill is a waterfall located in the city of Ubud, Indonesia. It is one of the most popular religious sites in the country, with over 20,000 visitors annually.`,
+    image: `./assets/images/bali-campuan-hill.webp`,
+    alt: "recomendation-bali-campuan-hill",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    title: "Customized Private Tours",
+    image: "./assets/icons/lock-cog.svg",
+    alt: "service-1",
+    description:
+      "Tailor-made itineraries with personalized guides, flexible schedules, and exclusive access to hidden gems.",
+  },
+  {
+    id: 2,
+    title: "Luxury Villa Stays",
+    image: "./assets/icons/home-star.svg",
+    alt: "service-2",
+    description:
+      "Exclusive accommodations in luxurious villas with private pools, personal chefs, and stunning views",
+  },
+  {
+    id: 3,
+    title: "Premium Transport Services",
+    image: "./assets/icons/car.svg",
+    alt: "service-3",
+    description:
+      "Comfortable and reliable transportation options, including private drivers and luxury vehicles",
+  },
+];
+
+export const testimonials = [
+  {
+    id: 1,
+    name: "Sarah",
+    role: "Web Developer",
+    avatar: "./assets/images/bali-besakih.webp",
+    avatarAlt: "avatar",
+    starIcon: "./assets/icons/star.svg",
+    starIconAlt: "star-icon",
+    text: "Top-notch service! The premium transport services made our trip so convenient and stress-free. Our driver was punctual, friendly, and the vehicle was super comfortable. Five stars!",
+  },
+  {
+    id: 2,
+    name: "Sarah",
+    role: "Web Developer",
+    avatar: "./assets/images/bali-besakih.webp",
+    avatarAlt: "avatar",
+    starIcon: "./assets/icons/star.svg",
+    starIconAlt: "star-icon",
+    text: "Top-notch service! The premium transport services made our trip so convenient and stress-free. Our driver was punctual, friendly, and the vehicle was super comfortable. Five stars!",
+  },
+  {
+    id: 3,
+    name: "Sarah",
+    role: "Web Developer",
+    avatar: "./assets/images/bali-besakih.webp",
+    avatarAlt: "avatar",
+    starIcon: "./assets/icons/star.svg",
+    starIconAlt: "star-icon",
+    text: "Top-notch service! The premium transport services made our trip so convenient and stress-free. Our driver was punctual, friendly, and the vehicle was super comfortable. Five stars!",
+  },
+];
